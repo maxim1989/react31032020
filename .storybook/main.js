79d0dee54aa341b2a44c2a95da0ef360a47c1f3a
Maxim <fedorov-max89@mail.ru>
2020-04-15 16:06:49 +0300

    Homework 09042020
    * homework_09042020. Подключить реакт, сторибук. Создать стори для компонента HelloWorld.
    
    * homework_09042020. Добавить юнит тест.
    
    * homework_09042020. Добавить loki.
    
    * homework_09042020. Добавить аддоны.
    
    * homework_09042020. HelloWorld сделать функциональным. Убрать из eslint валидацию PropTypes.

---
+++ .storybook/main.js
@@ -0,0 +1,23 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.[tj]s'],
+  addons: [
+    '@storybook/addon-storysource',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-notes/register'
+  ],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(js|jsx|ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [['@babel/react', { flow: false, typescript: true }]],
+      },
+    });
+
+    config.resolve.extensions.push('.ts', '.tsx', '.js', '.jsx');
+
+    return config;
+  },
+};
