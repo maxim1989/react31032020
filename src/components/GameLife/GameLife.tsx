d550abe6d6ea3df121b4ecefbd579810d7140711
Maksim <fedorov-max89@mail.ru>
2020-04-27 19:50:36 +0300

    homework_24042020. Добавить размеры поля.

--- src/components/GameLife/GameLife.tsx
+++ src/components/GameLife/GameLife.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import _ from 'lodash';
 import { css } from '@emotion/core';
+import styled from '@emotion/styled';
 
 import { FieldSizeEnum } from '@shared/enums';
 import { Cell, CellInterface } from './components/Cell';
 
+interface ButtonProps {
+    selected: boolean;
+    'data-size': number;
+}
+
+const Button = styled.button<ButtonProps>`
+  color: ${props => props.selected ? 'green' : 'red'};
+`;
+
+type FieldSize = FieldSizeEnum.Small | FieldSizeEnum.Medium | FieldSizeEnum.Big;
+
 interface FormInterface {
-    fieldSize: FieldSizeEnum.Small | FieldSizeEnum.Medium | FieldSizeEnum.Big;
+    fieldSize: FieldSize;
 };
 
 interface GameLifeProps {};
@@ -15,28 +28,65 @@ interface GameLifeState {
 };
 
 export class GameLife extends React.PureComponent<GameLifeProps, GameLifeState> {
-    state: GameLifeState = {
-        form: {
-            fieldSize: FieldSizeEnum.Small
-        },
-        data: [...new Array(FieldSizeEnum.Small * (FieldSizeEnum.Small - 20))].map((_, index) => ({
-            position: index
-        }))
+    constructor(props: GameLifeProps) {
+        super(props);
+
+        this.state = {
+            form: {
+                fieldSize: FieldSizeEnum.Small
+            },
+            data: this.createData(FieldSizeEnum.Small)
+        };
+    }
+
+    createData = (l: FieldSize):CellInterface[] => [...new Array(l * (l - 20))].map((_, index) => ({
+        position: index
+    }))
+
+    handleFieldSize = (event: React.MouseEvent<HTMLButtonElement>):void => {
+        event.preventDefault();
+        const fieldSize: number = _.toInteger(event.currentTarget.getAttribute('data-size'));
+
+        this.setState({ form: { fieldSize }, data: this.createData(fieldSize) });
     }
 
     render() {
         const { data, form: { fieldSize } } = this.state;
 
         return (
-            <div className="wrapper" css={css({
-                display: 'flex',
-                flexWrap: 'wrap',
-                width: `${fieldSize * 11}px`,
-                borderTop: '1px solid #000',
-                borderRight: '1px solid #000'
-            })}>
-                {data.map(({ position }) => <Cell key={position} position={position}/>)}
-            </div>
+            <>
+                <div css={css({
+                    marginBottom: '20px'
+                })}>
+                    <Button selected={fieldSize === FieldSizeEnum.Small}
+                            data-size={FieldSizeEnum.Small}
+                            onClick={this.handleFieldSize}
+                    >
+                        50x30
+                    </Button>
+                    <Button selected={fieldSize === FieldSizeEnum.Medium}
+                            data-size={FieldSizeEnum.Medium}
+                            onClick={this.handleFieldSize}
+                    >
+                        70x50
+                    </Button>
+                    <Button selected={fieldSize === FieldSizeEnum.Big}
+                            data-size={FieldSizeEnum.Big}
+                            onClick={this.handleFieldSize}
+                    >
+                        100x80
+                    </Button>
+                </div>
+                <div css={css({
+                     display: 'flex',
+                     flexWrap: 'wrap',
+                     width: `${fieldSize * 11}px`,
+                     borderTop: '1px solid #000',
+                     borderRight: '1px solid #000'
+                })}>
+                    {data.map(({ position }) => <Cell key={position} position={position}/>)}
+                </div>
+            </>
         );
     }
 } 
